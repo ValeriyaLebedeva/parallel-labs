d32dd1b667c354b870f60fc2ab317f69e64cab5a
ValeriyaLebedeva <lvs_vs@mail.ru>
2020-11-27 16:02:00 +0300

    Scripted auto-commit on change (2020-11-27 16:02:00) by gitwatch.sh

--- laba4/src/main/java/laba4/JsTest.java
+++ laba4/src/main/java/laba4/JsTest.java
@@ -1,4 +1,18 @@
 package laba4;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class JsTest {
+    public final static class User {
+        public final String name;
+        public final int age;
+        public final String countryOfResidence;
+        @JsonCreator
+        public User(@JsonProperty("name") String name, @JsonProperty("age") int age, @JsonProperty("countryOfRecidence") String countryOfResidence) {
+            this.name = name;
+            this.age = age;
+            this.countryOfResidence = countryOfResidence;
+        }
+    }
 }

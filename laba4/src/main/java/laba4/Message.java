46375c9d2f536faf1a19963112e0ec6f12cd2f39
ValeriyaLebedeva <lvs_vs@mail.ru>
2020-11-27 22:44:31 +0300

    Scripted auto-commit on change (2020-11-27 22:44:31) by gitwatch.sh

--- laba4/src/main/java/laba4/Message.java
+++ laba4/src/main/java/laba4/Message.java
@@ -1,4 +1,18 @@
 package laba4;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class Message {
+    public final String packageId;
+    public final String jsScript;
+    public final String functionName;
+    public final String tests;
+    @JsonCreator
+    public Message(@JsonProperty("packageId") String packageId, @JsonProperty("jsScript") String jsScript, @JsonProperty("functionName") String functionName, @JsonProperty("tests") String tests) {
+        this.packageId = packageId;
+        this.jsScript = jsScript;
+        this.functionName = functionName;
+        this.tests = tests;
+    }
 }

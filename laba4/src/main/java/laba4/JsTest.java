0171a3f903a95c8989d9ddfccd6cc4ed9aa9b540
ValeriyaLebedeva <lvs_vs@mail.ru>
2020-11-27 16:03:53 +0300

    Scripted auto-commit on change (2020-11-27 16:03:53) by gitwatch.sh

--- laba4/src/main/java/laba4/JsTest.java
+++ laba4/src/main/java/laba4/JsTest.java
@@ -9,7 +9,7 @@ public class JsTest {
         public final int age;
         public final String countryOfResidence;
         @JsonCreator
-        public User(@JsonProperty("packageId") String packageId, @JsonProperty("jsScript") int jsScript, @JsonProperty("countryOfRecidence") String countryOfResidence) {
+        public User(@JsonProperty("packageId") String packageId, @JsonProperty("jsScript") int jsScript, @JsonProperty("functionName") String countryOfResidence) {
             this.name = name;
             this.age = age;
             this.countryOfResidence = countryOfResidence;

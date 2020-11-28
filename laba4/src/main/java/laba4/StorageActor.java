13ef05901c3eaba3bfacd940952d897d4b15a782
ValeriyaLebedeva <lvs_vs@mail.ru>
2020-11-28 16:48:32 +0300

    Scripted auto-commit on change (2020-11-28 16:48:32) by gitwatch.sh

--- laba4/src/main/java/laba4/StorageActor.java
+++ laba4/src/main/java/laba4/StorageActor.java
@@ -55,6 +55,7 @@ public class StorageActor extends AbstractActor {
             System.out.println(msg.getTests().size());
             for (Test t: msg.getTests()) {
                 Future<Object> future = Patterns.ask(executorActors, new ExecuteTest(t, msg.getJsScript(), msg.getFunctionName()), timeout);
+                
                 Result result;
                 result = new Result(t.getTestName(), (String) Await.result(future, timeout.duration()), t.getExpectedResult());
                 System.out.printf("Executed test %s, result: %s%n", t.getTestName(), result.responseMsg);

9ac0ee735eb4c49fdb764de1716bfb0a2d827809
ValeriyaLebedeva <lvs_vs@mail.ru>
2020-12-21 01:23:18 +0300

    Scripted auto-commit on change (2020-12-21 01:23:18) by gitwatch.sh

--- laba5/src/main/java/laba5/TimeRequestTester.java
+++ laba5/src/main/java/laba5/TimeRequestTester.java
@@ -18,7 +18,7 @@ public class TimeRequestTester {
         ActorSystem system = ActorSystem.create("routes");
         final Http http = Http.get(system);
         final ActorMaterializer materializer = ActorMaterializer.create(system);
-        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = c
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = createFlow(materializer, )
         final CompletionStage<ServerBinding> binding = http.bindAndHandle(
                 routeFlow,
                 ConnectHttp.toHost("localhost", 8080),

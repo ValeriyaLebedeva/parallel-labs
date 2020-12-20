f355c61c008811ffba9ace746e1747750a36282a
ValeriyaLebedeva <lvs_vs@mail.ru>
2020-12-21 01:41:46 +0300

    Scripted auto-commit on change (2020-12-21 01:41:46) by gitwatch.sh

--- laba5/src/main/java/laba5/TimeRequestTester.java
+++ laba5/src/main/java/laba5/TimeRequestTester.java
@@ -9,14 +9,20 @@ import akka.http.javadsl.Http;
 import akka.http.javadsl.ServerBinding;
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
+import akka.http.javadsl.model.Query;
+import akka.pattern.Patterns;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
+import akka.util.Timeout;
+import java.time.Duration;
 import javafx.util.Pair;
 
 import java.io.IOException;
 import java.util.concurrent.CompletionStage;
+import java.util.regex.Pattern;
 
 public class TimeRequestTester {
+    private static  final Duration TIMEOUT = Duration.ofSeconds(2);
 
     public static void main(String[] args) throws IOException {
         System.out.println("start!");
@@ -40,11 +46,14 @@ public class TimeRequestTester {
     private static Flow<HttpRequest, HttpResponse, NotUsed> createFlow(ActorMaterializer materializer, ActorRef actorCashing) {
         return Flow.of(HttpRequest.class)
                 .map(r -> {
-
-                    return new Pair<>();
+                    Query q = r.getUri().query();
+                    Integer count = Integer.parseInt(q.get("count").get());
+                    System.out.println(count);
+                    return new Pair<>(q.get("testUrl").get(), count);
                 })
                 .mapAsync(1, (Pair<String, Integer> pair) -> {
-
+                    CompletionStage<Object> stage = Patterns.ask(actorCashing, new MessageGetResult(pair.getKey()), TIMEOUT);
+                    return stage.thenCompose((Object re))
                 })
     }
 

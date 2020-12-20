489672417338fec9b3e8a498d63604e1290bef0b
ValeriyaLebedeva <lvs_vs@mail.ru>
2020-12-21 01:14:53 +0300

    Scripted auto-commit on change (2020-12-21 01:14:53) by gitwatch.sh

--- laba5/src/main/java/laba5/CashingActor.java
+++ laba5/src/main/java/laba5/CashingActor.java
@@ -1,19 +1,20 @@
 package laba5;
 
 import akka.actor.AbstractActor;
+import akka.actor.ActorRef;
 import akka.japi.pf.ReceiveBuilder;
 
 import java.util.HashMap;
 
 
 public class CashingActor extends AbstractActor {
-    private final HashMap<String, Long> storage = new HashMap<>();
+    private final HashMap<String, float> storage = new HashMap<>();
 
     @Override
     public Receive createReceive() {
         return ReceiveBuilder.create()
                 .match(MessageGetResult.class, msg -> {
-                    getSender().tell(storage.getOrDefault(msg.getUrl(), ));
+                    getSender().tell(storage.getOrDefault(msg.getUrl(), (float)-1.0), ActorRef.noSender());
                 })
                 .match(MessageTest.class, msg -> {
 

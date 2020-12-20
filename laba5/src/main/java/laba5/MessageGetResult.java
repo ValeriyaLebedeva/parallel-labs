52ad0ebf6592c91da4addcb3fe5d82b1b39882ce
ValeriyaLebedeva <lvs_vs@mail.ru>
2020-12-21 01:02:38 +0300

    Scripted auto-commit on change (2020-12-21 01:02:38) by gitwatch.sh

--- laba5/src/main/java/laba5/MessageGetResult.java
+++ laba5/src/main/java/laba5/MessageGetResult.java
@@ -1,4 +1,11 @@
 package laba5;
 
 public class MessageGetResult {
+    private final String urlForTesting;
+    public MessageGetResult(String urlForTesting) {
+        this.urlForTesting = urlForTesting
+    }
+    public String getUrlForTesting() {
+        return urlForTesting;
+    }
 }

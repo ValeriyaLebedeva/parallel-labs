895c259fda6ac79a5f1f64f72b389f36e934d91f
ValeriyaLebedeva <lvs_vs@mail.ru>
2020-12-21 01:26:59 +0300

    Scripted auto-commit on change (2020-12-21 01:26:59) by gitwatch.sh

--- laba5/src/main/java/laba5/TimeRequestTester.java
+++ laba5/src/main/java/laba5/TimeRequestTester.java
@@ -1,15 +1,19 @@
 package laba5;
 
 import akka.NotUsed;
+import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
+import akka.actor.Props;
 import akka.http.javadsl.ConnectHttp;
 import akka.http.javadsl.Http;
+import akka.http.javadsl.ServerBinding;
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
 
 import java.io.IOException;
+import java.util.concurrent.CompletionStage;
 
 public class TimeRequestTester {
 
@@ -17,8 +21,9 @@ public class TimeRequestTester {
         System.out.println("start!");
         ActorSystem system = ActorSystem.create("routes");
         final Http http = Http.get(system);
+        ActorRef ActorCashing = system.actorOf(Props.create(CashingActor.class));
         final ActorMaterializer materializer = ActorMaterializer.create(system);
-        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = createFlow(materializer, )
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = createFlow(materializer, ActorCashing);
         final CompletionStage<ServerBinding> binding = http.bindAndHandle(
                 routeFlow,
                 ConnectHttp.toHost("localhost", 8080),
@@ -29,6 +34,10 @@ public class TimeRequestTester {
         binding
                 .thenCompose(ServerBinding::unbind)
                 .thenAccept(unbound -> system.terminate()); // and shutdown when done
+    }
+
+    private static Flow<HttpRequest, HttpResponse, NotUsed> createFlow(ActorMaterializer materializer, ActorRef actorCashing) {
+        
     }
 
 }

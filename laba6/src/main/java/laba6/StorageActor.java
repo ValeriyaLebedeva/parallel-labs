118e21e664a08feb0ae2976a27d81516eb9b6bf3
ValeriyaLebedeva <lvs_vs@mail.ru>
2020-12-22 17:29:15 +0300

    Scripted auto-commit on change (2020-12-22 17:29:15) by gitwatch.sh

--- laba6/src/main/java/laba6/StorageActor.java
+++ laba6/src/main/java/laba6/StorageActor.java
@@ -1,8 +1,14 @@
 package laba6;
 
+import akka.actor.AbstractActor;
+
 import java.util.ArrayList;
 
 public class StorageActor extends AbstractActor {
     ArrayList<String> servers = new ArrayList<>();
 
+    @Override
+    public Receive createReceive() {
+        return null;
+    }
 }

720b5ee723b4f276e079481eae43facd0e96e39f
ValeriyaLebedeva <lvs_vs@mail.ru>
2020-12-22 17:49:48 +0300

    Scripted auto-commit on change (2020-12-22 17:49:48) by gitwatch.sh

--- laba6/src/main/java/laba6/Anonimaizer.java
+++ laba6/src/main/java/laba6/Anonimaizer.java
@@ -1,4 +1,16 @@
 package laba6;
 
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+import akka.http.javadsl.Http;
+import akka.http.javadsl.model.HttpRequest;
+
 public class Anonimaizer {
+    public static int PORT;
+    public static void main(String[] argv) {
+        ActorSystem actorSystem = ActorSystem.create("routes");
+        Http http = Http.get(actorSystem);
+        actorSystem.actorOf(Props.create(StorageActor.class));
+        PORT = Integer.parseInt(argv[0]);
+    }
 }

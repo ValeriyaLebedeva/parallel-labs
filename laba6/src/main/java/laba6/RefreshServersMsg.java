e283c76c53f8ac63455669ab640f893936f5aaa1
ValeriyaLebedeva <lvs_vs@mail.ru>
2020-12-22 17:26:36 +0300

    Scripted auto-commit on change (2020-12-22 17:26:36) by gitwatch.sh

--- laba6/src/main/java/laba6/RefreshServersMsg.java
+++ laba6/src/main/java/laba6/RefreshServersMsg.java
@@ -1,4 +1,15 @@
 package laba6;
 
+import java.util.ArrayList;
+
 public class RefreshServersMsg {
+    private final ArrayList<String> servers;
+
+    public RefreshServersMsg(ArrayList<String> servers) {
+        this.servers = servers;
+    }
+
+    public ArrayList<String> getServers() {
+        return servers;
+    }
 }

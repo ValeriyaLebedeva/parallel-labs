e16146523f1c4f356debf20d47cde9570c932fba
ValeriyaLebedeva <lvs_vs@mail.ru>
2020-12-22 19:38:42 +0300

    Scripted auto-commit on change (2020-12-22 19:38:42) by gitwatch.sh

--- laba6/src/main/java/laba6/Anonymizer.java
+++ laba6/src/main/java/laba6/Anonymizer.java
@@ -41,7 +41,7 @@ public class Anonymizer {
             PORT = 2000 + random.nextInt(4000);
         }
         System.out.printf("Port: %d", PORT);
-        Zoo zoo = new Zoo(storageActor);
+        new Zoo(storageActor);
         final ActorMaterializer materializer = ActorMaterializer.create(actorSystem);
         final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow =
                 createRoute().flow(actorSystem, materializer);

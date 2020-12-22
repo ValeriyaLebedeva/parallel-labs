f077f1b2468e01fadcee657749655a957203fc25
ValeriyaLebedeva <lvs_vs@mail.ru>
2020-12-22 18:56:40 +0300

    Scripted auto-commit on change (2020-12-22 18:56:40) by gitwatch.sh

--- laba6/src/main/java/laba6/Anonymizer.java
+++ laba6/src/main/java/laba6/Anonymizer.java
@@ -6,13 +6,13 @@ import akka.actor.ActorSystem;
 import akka.actor.Props;
 import akka.http.javadsl.ConnectHttp;
 import akka.http.javadsl.Http;
-import akka.http.javadsl.IncomingConnection;
 import akka.http.javadsl.ServerBinding;
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
+import akka.http.javadsl.server.Route;
+import akka.pattern.Patterns;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
-
 import java.io.IOException;
 import java.util.concurrent.CompletionStage;
 
@@ -26,14 +26,13 @@ public class Anonymizer {
         PORT = Integer.parseInt(argv[0]);
         Zoo zoo = new Zoo(storage);
         final ActorMaterializer materializer = ActorMaterializer.create(actorSystem);
-        final Flow<HttpResponse, HttpRequest, NotUsed> routeFlow =
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow =
                 createRoute().flow(actorSystem, materializer);
         final CompletionStage<ServerBinding> binding = http.bindAndHandle(
                 routeFlow,
                 ConnectHttp.toHost(HOST, PORT),
                 materializer
         );
-        System.in.read();
         binding
                 .thenCompose(ServerBinding::unbind)
                 .thenAccept(unbound ->{
@@ -41,6 +40,7 @@ public class Anonymizer {
                 });
     }
 
-    private static IncomingConnection createRoute() {
+    private static Route createRoute() {
+        return;
     }
 }

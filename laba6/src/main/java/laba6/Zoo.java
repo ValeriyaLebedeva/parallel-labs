8f7de30c4f19c0556cebea21804c161c2f0f6ab2
ValeriyaLebedeva <lvs_vs@mail.ru>
2020-12-22 18:31:03 +0300

    Scripted auto-commit on change (2020-12-22 18:31:03) by gitwatch.sh

---
+++ laba6/src/main/java/laba6/Zoo.java
@@ -0,0 +1,37 @@
+package laba6;
+
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
+
+import java.io.IOException;
+import java.time.Duration;
+import java.util.ArrayList;
+
+public class Zoo {
+    public static final String ZOOKEEPER_ADDRESS = "localhost:2181";
+    private static final int TIMEOUT = (int)Duration.ofSeconds(5).getSeconds();
+    public static ZooKeeper zooKeeper;
+    public Zoo() throws IOException {
+        zooKeeper = new ZooKeeper(ZOOKEEPER_ADDRESS, TIMEOUT, watcher);
+    }
+
+    public static Watcher watcher = watchedEvent -> {
+        if (watchedEvent.getType() == Watcher.Event.EventType.NodeDataChanged ||
+                watchedEvent.getType() == Watcher.Event.EventType.NodeDeleted ||
+                watchedEvent.getType() == Watcher.Event.EventType.NodeDeleted) {
+            ArrayList<String> updatedServers = new ArrayList<>();
+            try {
+                for (String c: zooKeeper.getChildren("/servers", null)) {
+                    String port = new String(zooKeeper.getData("/servers/" + c, false, null));
+                    updatedServers.add(port);
+                }
+                Act
+            } catch (KeeperException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}

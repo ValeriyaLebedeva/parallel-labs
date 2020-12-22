81029c5829d30f813bb7f92e7bdfa974422b837c
ValeriyaLebedeva <lvs_vs@mail.ru>
2020-12-22 18:00:39 +0300

    Scripted auto-commit on change (2020-12-22 18:00:39) by gitwatch.sh

--- laba6/src/main/java/Zoo.java
+++ laba6/src/main/java/Zoo.java
@@ -1,6 +1,8 @@
+import java.time.Duration;
+
 public class Zoo {
     public static final String ZOOKEEPER_ADDRESS = "localhost:2181";
-
+    private static final Duration TIMEOUT = Duration.ofSeconds(5);
     public Zoo() {
         zooKeeper = new Zoo(ZOOKEEPER_ADDRESS, (int));
     }

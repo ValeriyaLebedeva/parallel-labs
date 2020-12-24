1a3b823ee80e34aae5aac42f5ffbeb6ab2df365e
ValeriyaLebedeva <lvs_vs@mail.ru>
2020-12-24 17:56:18 +0300

    Scripted auto-commit on change (2020-12-24 17:56:18) by gitwatch.sh

--- laba7/src/main/java/laba7/CacheData.java
+++ laba7/src/main/java/laba7/CacheData.java
@@ -1,6 +1,10 @@
 package laba7;
 
+import org.zeromq.ZContext;
+
 public class CacheData {
     private String id;
-    
+    private int left;
+    private int right;
+    private final ZContext zContext
 }
